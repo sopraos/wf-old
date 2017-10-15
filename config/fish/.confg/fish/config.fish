57ca7f85c9375a1c413c7b2cd1eac7d14663a897
Sopraos <sopraos@outlook.fr>
2017-10-15 14:11:24 +0200

    Fish shell configuration

---
+++ config/fish/.confg/fish/config.fish
@@ -0,0 +1,11 @@
+# Changer le message d'accueil?
+set fish_greeting ""
+
+# Titre Terminal
+#function fish_title; end; funcsave fish_title
+function fish_title
+	echo "Wadzaa Inc."
+end
+
+# THEME PURE #
+set fish_function_path /Users/sopraos/.config/fish/functions/theme-pure $fish_function_path
